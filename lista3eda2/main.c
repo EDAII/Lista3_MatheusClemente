ca47b9a6cf0cad3e63937e52d031c457386f4c91
Matheus Clemente <matheus_klementt@hotmail.com>
2018-09-10 21:18:15 -0300

    Initial commit

---
+++ lista3eda2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lista3eda2
+//
+//  Matheus Clemente Carvalho de Azevedo
+//  15/0042817
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
